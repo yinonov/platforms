2f013825d6178c1e86b62867b839bfcbf9121d2c
Yinon Oved <yinon@hotmail.com>
2025-04-29 18:56:03 +0300

    shoelace's in

--- src/app/pages/home-view/index.ts
+++ src/app/pages/home-view/index.ts
@@ -1,4 +1,5 @@
-// index.ts
+import "@shoelace-style/shoelace/dist/components/button/button.js";
+import "@shoelace-style/shoelace/dist/components/card/card.js";
 import { HomeView } from "./home-view";
 import { HomeViewTemplate as template } from "./home-view.template";
 import { HomeViewStyles as styles } from "./home-view.styles";
